95a2164ee5e16268426442798f0da89d83b685e4
Shin-Ogata <Shin.Ogata@jp.sony.com>
2017-06-13 17:04:30 +0900

    fixed: IMobileAppConfigration interface

--- src/generators/mobile/interfaces.ts
+++ src/generators/mobile/interfaces.ts
@@ -1,14 +1,34 @@
 ﻿import {
     IProjectConfigration,
     IBuildTargetConfigration,
+    IDependency,
 } from "../base";
+
+/**
+ * @interface IExternalModules
+ * @brief 外部ライブラリの定義
+ */
+export interface IExternalModules {
+    [name: string]: {
+        acquisition: "npm" | "npm:dev" | "cordova" | "resource";    // 取得先
+        regular: boolean;                                           // 既定でインストールに含めるか
+        subset?: IExternalModules;                                  // サブセットモジュール
+    };
+}
 
 /**
  * @interface IMobileAppConfigration
  * @brief mobile プロジェクトのコンフィギュレーション設定
  */
 export interface IMobileAppConfigration extends IProjectConfigration, IBuildTargetConfigration {
-    projectType: "mobile";  // プロジェクト種類
-    env?: "web";            // 実行環境の target
-    supportCSS: true;       // CSS を含める場合には true
+    projectType: "mobile";                  // プロジェクト種類
+    env?: "web";                            // 実行環境の target
+    appName: string;                        // アプリケーション名
+    appId?: string;                         // アプリケーション ID
+    platforms?: string[];                   // サポート OS プラットフォーム "android" | "ios"
+    projectStructure?: ["lib" | "porting"]; // サポートプロジェクト構成 "lib" | "porting"
+    external?: IExternalModules;            // プリセットに含める外部モジュール
+    dependencies?: IDependency[];           // npm 依存モジュール情報
+    cordova_plugin?: IDependency[];         // cordova plugin モジュール情報
+    resource_addon?: IDependency[];         // local resource モジュール情報
 }
